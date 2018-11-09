98c9b989edd4ff9f6453a3de1104671cc203df28
angeliquecd <acharles-davis00@stuy.edu>
2018-11-08 19:59:58 -0500

    added a few lines to work with diagonals and a few more lines to test horizontal etc

--- Tester.java
+++ Tester.java
@@ -2,6 +2,7 @@ public class Tester{
   public static void main(String[] args){
   WordSearch betap = new WordSearch(10,10);
   //tests toString
+  System.out.println("Testing initializing of grid.");
   System.out.println(betap);
   System.out.println("\n");
   //tests if word can be put in, should not return an error
@@ -11,7 +12,7 @@ public class Tester{
   System.out.println(betap);
   System.out.println("\n");
   //tests overlapping letters
-  System.out.println("adding yo horizontally: ");
+  System.out.println("adding yo horizontally (in an unacceptable place): ");
   betap.addWordHorizontal("yo",0,2);
   System.out.println(betap.addWordHorizontal("yo",0,2));
   System.out.println(betap);
@@ -33,7 +34,7 @@ public class Tester{
   System.out.println(betap);
   System.out.println("\n");
   //shouldn't work because spot is taken
-  System.out.println("adding hello vertically: ");
+  System.out.println("adding hello vertically (in an unacceptable place): ");
   betap.addWordVertical("hello",0,2);
   System.out.println(betap.addWordVertical("hello",0,2));
   System.out.println(betap);
@@ -43,5 +44,28 @@ public class Tester{
   betap.addWordVertical("antidisestablismentarianism",1,1);
   System.out.println(betap.addWordVertical("antidisestablismentarianism",0,1));
   System.out.println(betap);
+  System.out.println("Testing diagonal: ");
+  betap.clear();
+    System.out.println("adding hello diagonally: ");
+  System.out.println(  betap.addWordDiagonal("hey",0,0));
+  betap.addWordDiagonal("hey",0,0);
+  System.out.println(betap);
+  System.out.println("\n");
+    System.out.println("again: ");
+  System.out.println(betap.addWordDiagonal("hey",0,7));
+  betap.addWordDiagonal("hey",0,7);
+  System.out.println(betap);
+  System.out.println("adding hey out of bounds: ");
+System.out.println(betap.addWordDiagonal("hey",0,8));
+  betap.addWordDiagonal("hey",0,8);
+  System.out.println(betap);
+    System.out.println("adding hello diagonally (in an unacceptable place): ");
+    System.out.println(betap.addWordDiagonal("hey",2,2));
+    betap.addWordDiagonal("hey",2,2);
+System.out.println(betap);
+System.out.println("adding hello horizontally (in an unacceptable place): ");
+System.out.println(betap.addWordHorizontal("hey",2,2));
+betap.addWordHorizontal("hey",2,2);
+System.out.println(betap);
 }
 }
