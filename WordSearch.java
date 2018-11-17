9035258c01e9c19108ea7325e7eb69551d6b4d2c
angeliquecd <acharles-davis00@stuy.edu>
2018-11-16 20:37:00 -0500

    added comments to make code more legible

--- WordSearch.java
+++ WordSearch.java
@@ -1,13 +1,18 @@
+//Kiran Vuksanaj helped me with this lab, with conceptual stuff like the exceptions and random, but I didn't look at her code
 import java.util.*;
 import java.io.*;
 public class WordSearch{
-    private char[][] data;
-    private int seed;
+  private char[][] data;
+   //the random seed used to produce this WordSearch
+     private int seed;
+    //a random Object to unify your random calls
     private Random randgen;
+    //all words from a text file get added to wordsToAdd, indicating that they have not yet been added
     private ArrayList<String> wordsToAdd;
+    //all words that were successfully added get moved into wordsAdded.
     private ArrayList<String> wordsAdded;
-
-public WordSearch(int rows,int cols, String fileName, int Randseed, boolean key) throws FileNotFoundException{
+//constructor takes up to five parameters
+public WordSearch(int rows,int cols, String fileName, int Randseed, boolean key) throws FileNotFoundException{//constructs entire puzzle
       data= new char[rows][cols];
 	     clear();
        wordsToAdd= new ArrayList<String>();
@@ -28,13 +33,14 @@ if(!key){
   fillinletters();
 }
 }
-public void clear(){
+public void clear(){//fills in array with white space
 	for (int a = 0;a<data.length;a++){
 	for (int b = 0; b<data[0].length;b++){
 	data[a][b]=' ';
 	}}}
-
-  public String toString(){
+/**Prints out the puzzle.
+@return is 2D array of uppercase chars bounded by "|" and a list of hidden words and the seed at the bottom*/
+  public String toString(){//prints out the puzzle
       String puzzle="";
       for (int a =0;a<data.length;a++){
         puzzle+="|";
@@ -46,7 +52,20 @@ public void clear(){
       puzzle+= "Words: "+wordsAdded+"\n"+"Seed: "+this.seed;
     return puzzle;
         }
-    public boolean addWord(String word, int r, int c, int rowIncrement, int colIncrement){
+        /**Attempts to add a given word to the specified position of the WordGrid.
+  *The word is added in the direction rowIncrement,colIncrement
+  *Words must have a corresponding letter to match any letters that it overlaps.
+  *
+  *@param word is any text to be added to the word grid.
+  *@param row is the vertical locaiton of where you want the word to start.
+  *@param col is the horizontal location of where you want the word to start.
+  *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+  *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+  *@return true when: the word is added successfully.
+  *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
+  *        OR there are overlapping letters that do not match
+  */
+    public boolean addWord(String word, int r, int c, int rowIncrement, int colIncrement){//fits inside addallwords and places word where specified
       boolean therey= true;
       if(
       (r+word.length()>data.length && rowIncrement>0) || (r-word.length()<0 && rowIncrement<0)
@@ -63,7 +82,7 @@ public void clear(){
   else return false;
 }
 }
-private boolean doesitfit(String word, int r, int c, int rowIncrement, int colIncrement){
+private boolean doesitfit(String word, int r, int c, int rowIncrement, int colIncrement){//checks for colliding words
   for (int a = 0;a<word.length();a++){
     char space= data[r+a*rowIncrement][c+a*colIncrement];
     if (!(word.charAt(a)==space|| space==' ')){
@@ -81,8 +100,8 @@ private boolean doesitfit(String word, int r, int c, int rowIncrement, int colIn
 }*/
 public void addAllWords(){
   int limit= wordsToAdd.size();
-      for (int a = 0; a<limit;a++){
-        //printarray(wordsToAdd);
+      for (int a = 0; a<limit;a++){//how to make sure it tries each word
+        //printarray(wordsToAdd); debugging
         boolean added= false;
         int tries = 0;
       int index = Math.abs(randgen.nextInt()% wordsToAdd.size());
@@ -92,7 +111,7 @@ public void addAllWords(){
       int colIncrement=0;
       while ((rowIncrement==0 && colIncrement==0)){
       rowIncrement= randgen.nextInt()%2;
-      colIncrement= randgen.nextInt()%2;}
+      colIncrement= randgen.nextInt()%2;}//assigns word direction
       while (!added && tries<10000){
             int r = Math.abs(randgen.nextInt()%data.length);
             int c = Math.abs(randgen.nextInt()% data[0].length);
@@ -107,7 +126,7 @@ public void addAllWords(){
         }
       }
   }
-  public void fillinletters(){
+  public void fillinletters(){//fills in random letters into the puzzle
     for (int a =0;a<data.length;a++){
       for (int b=0;b<data[0].length;b++){
         if (data[a][b]==' '){
@@ -117,23 +136,25 @@ public void addAllWords(){
       }
     }
   }
-public static void main(String[]args){
+public static void main(String[]args){//runs program from terminal
   try{
   if (args.length<3) {
 printdirections("Need more information.");
 System.exit(1);}
-  if(args.length==3){
+  if(args.length==3){//row, col, filename give
       int rowbuilder= Integer.parseInt(args[0]);
       int colbuilder= Integer.parseInt(args[1]);
+      if (rowbuilder<=0||colbuilder<=0) throw new IllegalArgumentException();
       String filename= args[2];
     int seed = -10;
     boolean key = false;
      WordSearch a = new WordSearch(rowbuilder,colbuilder,filename,seed,key);
    System.out.println(a);
      }
-   if (args.length==4){
+   if (args.length==4){//row, col, filename, seed given
  	int rowbuilder= Integer.parseInt(args[0]);
     int colbuilder= Integer.parseInt(args[1]);
+    if (rowbuilder<=0||colbuilder<=0) throw new IllegalArgumentException();
       String filename= args[2];
     boolean key = false;
     int seed = Integer.parseInt(args[3]);
@@ -141,12 +162,13 @@ System.exit(1);}
       throw new IllegalArgumentException();}
  WordSearch a = new WordSearch(rowbuilder,colbuilder,filename,seed,key);
    System.out.println(a);}
-    if (args.length==5){
+    if (args.length>=5){//row, col, filename, seed and key given
  	int rowbuilder= Integer.parseInt(args[0]);
       int colbuilder= Integer.parseInt(args[1]);
+    if (rowbuilder<=0||colbuilder<=0) throw new IllegalArgumentException();
       String filename= args[2];
         int seed = Integer.parseInt(args[3]);
-        if (seed<0){
+        if (seed<0|| seed>10000){
           throw new IllegalArgumentException();}
 boolean key = false;
         if(args[4].equals("key")){
@@ -168,7 +190,7 @@ catch (FileNotFoundException b){//thrown in the constructor
 }
 
 public static void printdirections(String type){//in case of error in the terminal
-System.out.println(" You've encountered an error! ("+ type+") \n To start your program, input at least a number of rows, \n a number of columns and a file name in that order. \n On top of that you may input a seed (must be positive) \n and whether or not you want an answer key \n (If so mark: key), also in that order.");
+System.out.println(" You've encountered an error! ("+ type+") \n \n To start your program, input at least: \n 1.A positive number of rows, \n 2. A positive number of columns \n 3. The name of an existing file \n On top of that you may input: \n 4.A positive seed (must be less than 10,000) \n 5. Whether or not you want an answer key (If so mark: key) \n all in that order.");
 }
 
 }
